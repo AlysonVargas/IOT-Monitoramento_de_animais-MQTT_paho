bb449c0cd2df878d7b38a2e4eec2e69c7e0c918c
AlysonVargas <70177188+AlysonVargas@users.noreply.github.com>
2023-05-25 21:35:51 -0300

    query

--- mqtt-simulatorV2/mqtt-simulator/request.py
+++ mqtt-simulatorV2/mqtt-simulator/request.py
@@ -2,8 +2,9 @@ import paho.mqtt.client as mqtt
 import influxdb_client, os, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
+from flightsql import FlightSQLClient
 import pandas as pd
-import json
+
 
 
 token = os.environ.get("INFLUXDB_TOKEN")
@@ -13,6 +14,26 @@ token = token
 # Store the URL of your InfluxDB instance
 url="https://us-east-1-1.aws.cloud2.influxdata.com"
 
+'''
+
+query = """SELECT *
+FROM 'my_measurement'
+WHERE
+time >= now() - interval '1 hour'
+AND
+('BPM' IS NOT NULL OR 'peso' IS NOT NULL OR 'temp-amb' IS NOT NULL OR 'temp-ani' IS NOT NULL)"""
+
+# Define the query client
+query_client = FlightSQLClient(
+  host = url,
+  token = token,
+  metadata={"bucket-name": "Teste"})
+
+# Execute the query
+info = query_client.execute(query)
+reader = query_client.do_get(info.endpoints[0].ticket)
+'''
+
 client = influxdb_client.InfluxDBClient(
     url=url,
     token=token,
@@ -20,13 +41,15 @@ client = influxdb_client.InfluxDBClient(
 )
 
 
-query = f'from(bucket:"{bucket}") |> range(start: -5h) |> filter(fn: (r) => r._measurement == "Sensores")'
+query = f'from(bucket: "Teste")|> range(start: -1h)'
 tables = client.query_api().query(query)
 
-
-
-
-
+'''
+# Convert to dataframe
+data = reader.read_all()
+df = data.to_pandas().sort_values(by="time")
+print(df)
+'''
 
 for table in tables:
         for record in table.records:
